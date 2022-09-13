5c70f40672c97dc0ddbc5a459c475477d2115b01
William <wms274@nyu.edu>
2022-09-13 13:00:13 -0400

    css h3 fix

--- background.js
+++ background.js
@@ -22,9 +22,12 @@ fetch('https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&ho
     let isRaining = obj.hourly.precipitation[parseInt(today.getHours())]>0.05
     console.log(isRaining)
 
-
-    if (currentTime>sunrise && currentTime<sunset) {
+    if (currentTime>sunrise && currentTime<sunset && isRaining) {
         body.style = 'background-image: url(./weatherAppPhotos/RainDay.png)'
+    } else if (currentTime>sunrise && currentTime<sunset && !isRaining) {
+        body.style = 'background-image: url(./weatherAppPhotos/SunnyDay.png)'
+    } else if (currentTime<sunrise || currentTime>sunset && isRaining) {
+        body.style = 'background-image: url(./weatherAppPhotos/RainNightTwo.png)'
     } else {
         body.style = 'background-image: url(./weatherAppPhotos/ClearNightTwo.png)'
     }
