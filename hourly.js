aaac8392f8088ca9b387a22dfb6ccdc87a11fbef
William <wms274@nyu.edu>
2022-09-13 15:22:32 -0400

    hourly added

--- hourly.js
+++ hourly.js
@@ -1,19 +1,30 @@
+
+let hour = parseInt(today.getHours())
+
+
 fetch('https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m,relativehumidity_2m,precipitation,windspeed_10m,winddirection_10m&daily=sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York')
 .then(res => res.json())
 .then(obj => {
-    for (i=0; i<=24; i++){
-        renderTemp(obj.hourly["temperature_2m"][i], i)
+    for (i=0; i<6; i++){
+        renderTemp(obj.hourly["temperature_2m"][hour], i)
+        renderHour(hour, i)
+        hour++
     }
 })
 
 
-function renderTemp(hour, i) {
-    const hrLst = document.createElement('ul')
-    const hourTemp = document.createElement('li')
-    hourTemp.textContent = `${i}:00  |  ${hour}F`
+function renderTemp(obj, i) {
 
-    const hourlyTempDiv = document.querySelector('#temp')
+    console.log(document.getElementsByClassName('list')[i])
+    const hourTemp = document.getElementsByClassName('list')[i]
+    hourTemp.textContent = `${obj}°`
 
-    hrLst.append(hourTemp)
-    hourlyTempDiv.append(hrLst)
+}
+
+function renderHour(hour, i) {
+
+    console.log(document.getElementsByClassName('hourList')[i])
+    const hourTemp = document.getElementsByClassName('hourList')[i]
+    hourTemp.textContent = `${hour}:00`
+
 }
